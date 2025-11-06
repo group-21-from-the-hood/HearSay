a397d9fe3fbb61c75412ddbf86ff7121b09dad03
manofthebees <jibran.syed77@gmail.com>
2025-11-06 04:33:47 -0500

    new file:   hearsay/.gitignore 	new file:   hearsay/README.md 	new file:   hearsay/eslint.config.js 	new file:   hearsay/index.html 	new file:   hearsay/package-lock.json 	new file:   hearsay/package.json 	new file:   hearsay/public/vite.svg 	renamed:    src/placeholder -> hearsay/src/App.css 	new file:   hearsay/src/App.jsx 	new file:   hearsay/src/assets/react.svg 	new file:   hearsay/src/index.css 	new file:   hearsay/src/main.jsx 	new file:   hearsay/vite.config.js

---
+++ hearsay/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <p className="text-center text-green-500 font-bold text-9xl">
+      Hello World!
+    </p>
+  );
+}
+
+export default App
