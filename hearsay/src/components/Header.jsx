b3f20604ceb311be5bb90f64aa10b5f475430327
Hashir <hashir.usman@ontariotechu.net>
2025-11-10 20:22:20 -0500

    modified:   hearsay/index.html 	modified:   hearsay/src/components/Header.jsx

--- hearsay/src/components/Header.jsx
+++ hearsay/src/components/Header.jsx
@@ -8,13 +8,13 @@ export default function Header() {
   return (
     <header className="border-b-2 border-black dark:border-white bg-white dark:bg-gray-900 fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4 py-3 flex items-center justify-between">
-        <Link to="/" className="text-xl font-bold hover:underline text-black dark:text-white">LOGO</Link>
+        <Link to="/" className="text-xl font-bold hover:underline text-black dark:text-white">HearSay</Link>
         
         <nav className="flex-1 mx-8">
           <ul className="flex justify-center space-x-8">
-            <li><Link to="/my-reviews" className="hover:underline text-black dark:text-white">my reviews</Link></li>
-            <li><Link to="/discover" className="hover:underline text-black dark:text-white">discover</Link></li>
-            <li><Link to="/random" className="hover:underline text-black dark:text-white">random</Link></li>
+            <li><Link to="/my-reviews" className="hover:underline text-black dark:text-white">My Reviews</Link></li>
+            <li><Link to="/discover" className="hover:underline text-black dark:text-white">Discover</Link></li>
+            <li><Link to="/random" className="hover:underline text-black dark:text-white">Random</Link></li>
           </ul>
         </nav>
 
@@ -28,14 +28,14 @@ export default function Header() {
           </button>
           <input
             type="search"
-            placeholder="search"
+            placeholder="Search"
             className="border-2 border-black dark:border-white px-3 py-1 bg-white dark:bg-gray-800 text-black dark:text-white"
           />
           <button 
             onClick={() => navigate('/auth')}
             className="border-2 border-black dark:border-white px-4 py-1 hover:bg-gray-100 dark:hover:bg-gray-800 bg-white dark:bg-gray-900 text-black dark:text-white"
           >
-            login/register
+            Login/Register
           </button>
         </div>
       </div>
